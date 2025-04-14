3e495e96ea9788b9b733ffb4064bef978322792a
Vaishnavi <uw.vgaikwad24@gmail.com>
2025-04-14 16:15:35 -0700

    Added constants and image declaration

---
+++ image.d.ts
@@ -0,0 +1,26 @@
+//Declare different typescript rules
+
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: any;
+  export default value;
+}
